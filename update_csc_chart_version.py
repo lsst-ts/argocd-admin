41e56596a4745796a521b116a4ccf7d1e3b46208
Michael Reuter <mareuter@lsst.org>
2021-01-13 18:00:16 -0700

    Add script to update version for apps using the csc Helm chart.

---
+++ update_csc_chart_version.py
@@ -0,0 +1,37 @@
+import argparse
+import pathlib
+
+import yaml
+
+APPS_DIR = "apps"
+
+def main(opts):
+    print(f"Updating apps using csc Helm chart to version {opts.chart_version}")
+    apps = pathlib.PosixPath(APPS_DIR)
+    dirlist = list(apps.iterdir())
+    for appdir in dirlist:
+        requirements = appdir / "requirements.yaml"
+
+        if not requirements.exists():
+            continue
+
+        with requirements.open() as ifile:
+            values = yaml.safe_load(ifile)
+
+        dependencies = values["dependencies"]
+        for dependency in dependencies:
+            if dependency["name"] == "csc":
+                dependency["version"] = opts.chart_version
+
+        # print(appdir, values)
+
+        with requirements.open("w") as ofile:
+            yaml.dump(values, ofile, sort_keys=False)
+
+if __name__ == '__main__':
+    description = ["Update version for apps using the csc Helm chart"]
+    parser = argparse.ArgumentParser(description=" ".join(description),
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("chart_version", help="The version of the csc Helm chart to set.")
+    args = parser.parse_args()
+    main(args)
